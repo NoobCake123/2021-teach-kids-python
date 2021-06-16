52b360bfc55a0222d3d4d077f66efe9a80e094dd
noobcake <rui.kong@gmail.com>
2021-06-15 20:21:20 -0700

    blah blah

--- naemshape-generator.py
+++ naemshape-generator.py
@@ -9,7 +9,7 @@ colors = ["red", "yellow", "blue", "green", "orange", "aqua"]
 for x in range(sides):
     t.pencolor(colors[x % 6])
     t.penup()
-    t.forward(20)
+    t.forward(5*len(your_name)+2)
     t.pendown()
     t.write(your_name)
     t.left(180 - ((sides * 180 - 360.0) / sides))
