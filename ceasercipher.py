89509694f4e48b170757838f0ec39a2bebf89f6f
Noob Cake <tulobei@gmail.com>
2021-06-16 10:26:04 -0700

    hi

---
+++ ceasercipher.py
@@ -0,0 +1,28 @@
+code = input("do you want to encode or decode? Press e for encode, or d for decode")
+if (code == ("e")):
+    message = input("what do you want to encode?")
+    cipher = int(input("how many letters do you want to shift?"))
+    message = message.upper()
+    output = ""
+    for letter in message:
+        if letter.isupper:
+            value = ((ord(letter) - 65 + cipher) % 26) + 65
+            letter = chr(value)
+            output += letter
+    print("output message is", output)
+if (code == ("d")):
+   message = input("what is your encoded message?")
+   cipher = int(input("how many letters is the message shifted by?"))
+   message = message.upper()
+   output = ""
+   for letter in message:
+       if letter.isupper:
+           if ord(letter) < 65+cipher:
+               value = 90 - (ord(letter)-65)
+           else:
+               value = (ord(letter)-cipher)
+        letter = chr(value)
+
+
+
+
--- shape-generator.py
+++ shape-generator.py
@@ -1,13 +1,13 @@
 import turtle
 
 t = turtle.Pen()
-t.speed(0)
+t.speed(1)
 a = int(input("side length"))
 b = int(input("amount of sides"))
-c = (b * 180 - 360.0) / b
+c = (b * 181 - 360.0) / b
 print(c)
 for x in range(b):
     t.forward(a)
-    t.right(180-((b * 180 - 360.0) / b))
+    t.right(181-((b * 180 - 360.0) / b))
 input("are you ready to end it?")
 # i hope this works
