c9228ed81441cfe3107e7c167d49f1a29a04bd91
Noob Cake <tulobei@gmail.com>
2021-06-16 07:45:18 -0700

    hi

--- naemmaker.py
+++ naemmaker.py
@@ -1,14 +1,15 @@
 import turtle
 
 t = turtle.Pen()
-t.speed(0)
+t.speed(0.5)
 turtle.bgcolor("black")
 your_name = turtle.textinput("Enter you name", "what is your name")
 colors = ["red", "yellow", "blue", "green", "orange", "aqua"]
-for x in range(420):
+for x in range(100):
     t.pencolor(colors[x % 6])
     t.penup()
-    t.forward(x * 3.6)
+    t.forward((x * 6))
     t.pendown()
-    t.write(your_name)
-    t.left(69)
+    t.write(your_name, font=("Times", int(x + 6 / 6), "bold"))
+    t.left(63)
+input("are you ready to stop looking at your creation?")
