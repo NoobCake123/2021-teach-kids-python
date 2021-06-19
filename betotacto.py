61e98a86c4ce915cab95270f08ebaa08a3e7138b
Noob Cake <tulobei@gmail.com>
2021-06-19 12:23:26 -0700

    hi

--- betotacto.py
+++ betotacto.py
@@ -29,17 +29,28 @@ c2valid = 0
 c3valid = 0
 
 
+def is_game_going():
+    global game_is_going
+    if a1 in ['X', 'O'] and a2 in ['X', 'O'] and a3 in ['X', 'O'] and b1 in ['X', 'O'] and b2 in ['X', 'O'] and b3 in [
+        'X', 'O'] and c1 in ['X', 'O'] and c2 in ['X', 'O'] and c3 in ['X', 'O']:
+        game_is_going = 0
+    if a1valid == 2 and a2valid == 2 and a3valid == 2 or a1valid == 2 and b1valid == 2 and c1valid == 2 or a1valid == 2 and b2valid == 2 and c3valid == 2 or a3valid == 2 and b2valid == 2 and c1valid == 2 or a2valid == 2 and b2valid == 2 and c2valid == 2 or a3valid == 2 and b3valid == 2 and c3valid == 2 or b1valid == 2 and b2valid == 2 and b3valid == 2 or c1valid == 2 and c2valid == 2 and c3valid == 2:
+        game_is_going = 2
+    if a1valid == 1 and a2valid == 1 and a3valid == 1 or a1valid == 1 and b1valid == 1 and c1valid == 1 or a1valid == 1 and b2valid == 1 and c3valid == 1 or a3valid == 1 and b2valid == 1 and c1valid == 1 or a2valid == 1 and b2valid == 1 and c2valid == 1 or a3valid ==1 and b3valid == 1 and c3valid == 1 or b1valid == 1 and b2valid == 1 and b3valid == 1 or c1valid == 1 and c2valid == 1 and c3valid == 1:
+        game_is_going = 3
+
 
 printictac = print_tac_toe(a1, a2, a3, b1, b2, b3, c1, c2, c3)
 while game_is_going == 1:
     if turns % 2 == 0:
-        game_is_going = 0
+        is_game_going()
+        print(game_is_going)
         turns = turns + 1
         move = input("player 1 what is your move?")
         if move == 'a1':
             if a1valid == 0:
                 a1 = 'X'
-                a1valid = a1valid + 1
+                a1valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
@@ -47,56 +58,56 @@ while game_is_going == 1:
         elif move == 'a2':
             if a2valid == 0:
                 a2 = 'X'
-                a2valid = a2valid + 1
+                a2valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
         elif move == 'a3':
             if a3valid == 0:
                 a3 = 'X'
-                a3valid = a3valid + 1
+                a3valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
         elif move == 'b1':
             if b1valid == 0:
                 b1 = 'X'
-                b1valid = b1valid + 1
+                b1valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
         elif move == 'b2':
             if b2valid == 0:
                 b2 = 'X'
-                b2valid = b2valid + 1
+                b2valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
         elif move == 'b3':
             if b3valid == 0:
                 b3 = 'X'
-                b3alid = b3valid + 1
+                b3valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
         elif move == 'c1':
             if c1valid == 0:
                 c1 = 'X'
-                c1valid = c1valid + 1
+                c1valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
         elif move == 'c2':
             if c2valid == 0:
                 c2 = 'X'
-                c2valid = c2valid + 1
+                c2valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
         elif move == 'c3':
             if c3valid == 0:
                 c3 = 'X'
-                c3valid = c3valid + 1
+                c3valid = 2
             else:
                 print('this square is taken, please make a different move')
                 turns = 0
@@ -106,6 +117,8 @@ while game_is_going == 1:
             print("player 1 sucks")
     print_tac_toe(a1, a2, a3, b1, b2, b3, c1, c2, c3)
     if turns % 2 == 1:
+        print(game_is_going)
+        is_game_going()
         turns = turns + 1
         move = input("player 2 what is your move?")
         if move == 'a1':
@@ -178,4 +191,11 @@ while game_is_going == 1:
     print_tac_toe(a1, a2, a3, b1, b2, b3, c1, c2, c3)
 if game_is_going == 0:
     print("It's a tie...")
+    print("EVERYONE SUCKS GOODBYE")
     quit()
+elif game_is_going == 2:
+    print("WOOOOO pLAYER 1 wOn")
+elif game_is_going == 3:
+    print("WOOOOO pLAYER 2 wOn")
+else:
+    pass
---
+++ tst.py
@@ -0,0 +1,4 @@
+a1 = 'P'
+a2 = 'X'
+if (a1 and a2) in ['X', 'O']:
+    print('it works')
